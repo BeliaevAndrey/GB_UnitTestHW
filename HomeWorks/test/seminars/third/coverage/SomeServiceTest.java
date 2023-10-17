ddc07df4acf547fbb53417eda746f5e4b8e9de0d
Andrei Beliaev <ab5030107021@yandex.ru>
2023-10-17 18:02:29 +0300

    HomeWork03: seminar-work added

--- HomeWorks/test/seminars/third/coverage/SomeServiceTest.java
+++ HomeWorks/test/seminars/third/coverage/SomeServiceTest.java
@@ -10,23 +10,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SomeServiceTest {
-   // 3.1.
-   SomeService service;
+    // 3.1.
+    SomeService service;
+
     @BeforeEach
-    void setUp(){
+    void setUp() {
         service = new SomeService();
     }
 
     @ParameterizedTest
-    @ValueSource(ints ={3,6,9,12})
+    @ValueSource(ints = {3, 6, 9, 12})
     void multipleThreeNotFiveReturnsFizz(int input) {
-         String actual = service.fizzBuzz(input);
+        String actual = service.fizzBuzz(input);
 
-         assertThat(actual).isEqualTo("Fizz");
+        assertThat(actual).isEqualTo("Fizz");
     }
 
     @ParameterizedTest
-    @ValueSource(ints ={5,10,20,25})
+    @ValueSource(ints = {5, 10, 20, 25})
     void multipleFiveNotThreeReturnsBuzz(int input) {
         String actual = service.fizzBuzz(input);
 
@@ -34,7 +35,7 @@ class SomeServiceTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints ={15,30,45,60})
+    @ValueSource(ints = {15, 30, 45, 60})
     void multipleFiveWithThreeReturnsFizzBuzz(int input) {
         String actual = service.fizzBuzz(input);
 
@@ -42,7 +43,7 @@ class SomeServiceTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints ={14,31,46,61})
+    @ValueSource(ints = {14, 31, 46, 61})
     void multipleNoFiveNoThreeReturnsValue(int input) {
         String actual = service.fizzBuzz(input);
         assertThat(actual).isEqualTo(String.valueOf(input));
@@ -50,86 +51,102 @@ class SomeServiceTest {
 
     // 3.2
     @Test
-    void checkNullableArray(){
+    void checkNullableArray() {
         boolean actual = service.firstLast6(null);
         assertFalse(actual);
     }
 
     @Test
-    void checkEmptyArray(){
+    void checkEmptyArray() {
         boolean actual = service.firstLast6(new int[]{});
         assertFalse(actual);
     }
 
     @Test
-    void checkWithout6Array(){
-        boolean actual = service.firstLast6(new int[]{0,0,0});
+    void checkWithout6Array() {
+        boolean actual = service.firstLast6(new int[]{0, 0, 0});
         assertFalse(actual);
     }
 
     @Test
-    void checkFirst6Array(){
-        boolean actual = service.firstLast6(new int[]{6,0,0});
-        assertTrue(actual);
-    }
-    @Test
-    void checkLast6Array(){
-        boolean actual = service.firstLast6(new int[]{0,0,6});
+    void checkFirst6Array() {
+        boolean actual = service.firstLast6(new int[]{6, 0, 0});
         assertTrue(actual);
     }
 
     @Test
-    void checkMiddle6Array(){
-        boolean actual = service.firstLast6(new int[]{0,6,0});
+    void checkLast6Array() {
+        boolean actual = service.firstLast6(new int[]{0, 0, 6});
+        assertTrue(actual);
+    }
+
+    @Test
+    void checkMiddle6Array() {
+        boolean actual = service.firstLast6(new int[]{0, 6, 0});
         assertFalse(actual);
     }
 
     @Test
-    void checkAndFirstAndLast6Array(){
-        boolean actual = service.firstLast6(new int[]{6,0,6});
+    void checkAndFirstAndLast6Array() {
+        boolean actual = service.firstLast6(new int[]{6, 0, 6});
         assertTrue(actual);
     }
+
+    @Test
+    void testNullArrayFirstLast6() {
+        assertFalse(service.firstLast6(null));
+    }
+
+    @Test
+    void testEmptyArrayFirstLast6() {
+        assertFalse(service.firstLast6(new int[0]));
+    }
+
+    // 3.3
+
     /******************************************************/
     @ParameterizedTest
     @CsvSource(value = {"100, 50", "200, 50", "1000, 20"})
-    void checkCalculateDiscountPositive(double purchase, int discount){
+    void checkCalculateDiscountPositive(double purchase, int discount) {
         double actual = service.calculatingDiscount(purchase, discount);
         assertThat(actual).isEqualTo(purchase - purchase * discount / 100);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"-100, 50", "-200, 50", "-1000, 20"})
-    void checkCalculateDiscountWithNegPurchaseNegative(double purchase, int discount){
+    void checkCalculateDiscountWithNegPurchaseNegative(double purchase, int discount) {
         Exception exc = assertThrows(IllegalArgumentException.class,
-                ()-> service.calculatingDiscount(purchase, discount));
+                () -> service.calculatingDiscount(purchase, discount));
         assertEquals(exc.getMessage(), "purchase can't be less than zero");
     }
+
     @ParameterizedTest
     @CsvSource(value = {"100, -50", "200, -50", "1000, -20"})
-    void checkCalculateDiscountWithNegDiscNegative(double purchase, int discount){
+    void checkCalculateDiscountWithNegDiscNegative(double purchase, int discount) {
         Exception exc = assertThrows(IllegalArgumentException.class,
-                ()-> service.calculatingDiscount(purchase, discount));
+                () -> service.calculatingDiscount(purchase, discount));
         assertEquals(exc.getMessage(), "discount can't be less than zero");
     }
 
     @ParameterizedTest
     @CsvSource(value = {"100, 150", "200, 150", "1000, 120"})
-    void checkCalculateDiscountWithBigDiscountNegative(double purchase, int discount){
+    void checkCalculateDiscountWithBigDiscountNegative(double purchase, int discount) {
         Exception exc = assertThrows(IllegalArgumentException.class,
-                ()-> service.calculatingDiscount(purchase, discount));
+                () -> service.calculatingDiscount(purchase, discount));
         assertEquals(exc.getMessage(), "discount can't be greater than zero");
     }
 
-    /******************************************************/
+    // 3.4
 
+    /******************************************************/
     @Test
-    void checkSumThreeNumbersPositiveResult(){
+    void checkSumThreeNumbersPositiveResult() {
         int actual = service.calculatingSumThreeNumbers(1, 2, 3);
         assertThat(actual).isEqualTo(6);
     }
 
     @Test
-    void checkSumThreeNumbersNegativeResult(){
+    void checkSumThreeNumbersNegativeResult() {
         assertThat(service.calculatingSumThreeNumbers(13, 2, 3))
                 .isEqualTo(5);
         assertThat(service.calculatingSumThreeNumbers(1, 2, 13))
@@ -148,4 +165,11 @@ class SomeServiceTest {
                 .isEqualTo(0);
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {
+            "13, 2, 3, 5",  "1, 2, 13, 3",  "1, 13, 3, 4",  "13, 2, 13, 2",
+            "1, 13, 13, 1",  "13, 13, 3, 3",  "13, 13, 13, 0"})
+    void checkSumThreeNumbersNegativeResultP(int a, int b, int c, int e) {
+        assertEquals(e, service.calculatingSumThreeNumbers(a, b, c));
+    }
 }
