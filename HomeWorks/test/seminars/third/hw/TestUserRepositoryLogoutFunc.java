7cc5fec8f5d0eb4fbd94e0e17cd8c0e28c60e66e
Andrei Beliaev <ab5030107021@yandex.ru>
2023-10-17 21:42:38 +0300

    HomeWork03.3: UserRepository.logoutAllButAdm() -- test added

--- HomeWorks/test/seminars/third/hw/TestUserRepositoryLogoutFunc.java
+++ HomeWorks/test/seminars/third/hw/TestUserRepositoryLogoutFunc.java
@@ -2,19 +2,59 @@ package seminars.third.hw;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import seminars.third.tdd.User;
 import seminars.third.tdd.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class TestUserRepositoryLogoutFunc {
 
     UserRepository userRepository;
+    List<User> testUsers;
+    List<User> testAdmins;
 
     @BeforeEach
     void setUp() {
         userRepository = new UserRepository();
+        testUsers = new ArrayList<>();
+        testAdmins = new ArrayList<>();
+
+        String uName = "User";
+        String uPass = "uSecret";
+
+        String aName = "AdUser";
+        String aPass = "aSecret";
+        for (int i = 0; i < 5; i++) {
+            User user = new User(uName + i, uPass, false);
+            user.authenticate(uName + i, uPass);
+            User admin = new User(aName + i, aPass, true);
+            admin.authenticate(aName + i, aPass);
+            userRepository.addUser(user);
+            testUsers.add(user);
+            userRepository.addUser(admin);
+            testAdmins.add(admin);
+        }
     }
 
     @Test
     void testLogoutAllButAdm(){
+        String uName = "User";
+        String aName = "AdUser";
+        for (int i = 0; i < 5; i++) {
+            assertTrue(userRepository.findByName(uName + i));
+            assertTrue(userRepository.findByName(aName + i));
+        }
+
+        userRepository.logoutAllButAdm();
+
+        for (int i = 0; i < 5; i++) {
+            assertFalse(userRepository.findByName(uName + i));
+            assertTrue(userRepository.findByName(aName + i));
+        }
 
     }
 }
