10c00bacb82bf4847a6100c94d0a3d13c355a2c0
Andrei Beliaev <ab5030107021@yandex.ru>
2023-10-20 21:48:24 +0300

    HomeWork04: 4.2 some tests performed

--- HomeWorks/test/seminars/fourth/hw/book/BookServiceTest.java
+++ HomeWorks/test/seminars/fourth/hw/book/BookServiceTest.java
@@ -1,7 +1,32 @@
 package seminars.fourth.hw.book;
 
+import org.junit.jupiter.api.Test;
+import seminars.fourth.book.Book;
+import seminars.fourth.book.BookRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.nullable;
+import static org.mockito.Mockito.*;
 
 class BookServiceTest {
+    @Test
+    void testBookService() {
+        String testBookId = "123";
+        BookRepository bookRepository = mock(BookRepository.class);
+        when(bookRepository.findById(testBookId)).thenReturn(new Book(testBookId));
+        when(bookRepository.findAll()).thenReturn(new ArrayList<>(){{
+            add(new Book("123"));
+            add(new Book("124"));
+            add(new Book("125"));
+        }});
+        bookRepository.findAll();
+        verify(bookRepository, times(1)).findAll();
+        bookRepository.findById("123");
+        verify(bookRepository, times(1)).findById(anyString());
 
+
+    }
 }
