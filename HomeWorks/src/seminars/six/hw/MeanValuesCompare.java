b8377e84f60d2cd68af11cc8eabbb8aab86dc476
Andrei Beliaev <ab5030107021@yandex.ru>
2023-10-29 23:07:35 +0300

    HomeWork06: constants named according to SonarLint advice; static modifiers added;

--- HomeWorks/src/seminars/six/hw/MeanValuesCompare.java
+++ HomeWorks/src/seminars/six/hw/MeanValuesCompare.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 public class MeanValuesCompare<T> {
 
-    private final String ANSWER_FIRST = "Первый список имеет большее среднее значение";
-    private final String ANSWER_SECOND = "Второй список имеет большее среднее значение";
-    private final String ANSWER_EQUALS = "Средние значения равны";
+    private static final String ANSWER_FIRST = "Первый список имеет большее среднее значение";
+    private static final String ANSWER_SECOND = "Второй список имеет большее среднее значение";
+    private static final String ANSWER_EQUALS = "Средние значения равны";
 
 
     public String compare(List<T> first, List<T> second) {
@@ -14,7 +14,8 @@ public class MeanValuesCompare<T> {
         double firstMean = getMeanOfList(first);
         double secondMean = getMeanOfList(second);
         double mark = firstMean - secondMean;
-        return mark > 0 ? ANSWER_FIRST : mark < 0 ? ANSWER_SECOND : ANSWER_EQUALS;
+        if (mark == 0) return ANSWER_EQUALS;
+        return mark > 0 ? ANSWER_FIRST : ANSWER_SECOND;
     }
 
     private double getMeanOfList(List<T> values) {
