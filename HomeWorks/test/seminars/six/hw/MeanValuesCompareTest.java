c5808b2cbe97397921dc9b6048609bad65222c03
Andrei Beliaev <ab5030107021@yandex.ru>
2023-10-29 08:32:40 +0300

    HomeWork06: Tests drafts added

--- HomeWorks/test/seminars/six/hw/MeanValuesCompareTest.java
+++ HomeWorks/test/seminars/six/hw/MeanValuesCompareTest.java
@@ -1,6 +1,10 @@
 package seminars.six.hw;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,4 +15,30 @@ class MeanValuesCompareTest {
     void setUp() {
         meanValuesCompare = new MeanValuesCompare();
     }
+
+    @Test
+    void testCompareFirstGreaterSuccess() {
+        String expected = "Первый список имеет большее среднее значение";
+        ArrayList<Integer> first = new ArrayList<>(Arrays.asList(11, 12, 13, 14, 15));
+        ArrayList<Integer> second = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        String actual = meanValuesCompare.compare(first, second);
+    }
+
+    @Test
+    void testCompareSecondGreaterSuccess() {
+        String expected = "Второй список имеет большее среднее значение";
+        ArrayList<Integer> first = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        ArrayList<Integer> second = new ArrayList<>(Arrays.asList(11, 12, 13, 14, 15));
+        String actual = meanValuesCompare.compare(first, second);
+    }
+
+    @Test
+    void testCompareMeanValuesEquals() {
+        String expected = "Средние значения равны";
+        ArrayList<Integer> first = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        ArrayList<Integer> second = new ArrayList<>(Arrays.asList(4, 5, 2, 1, 3));
+        String actual = meanValuesCompare.compare(first, second);
+
+    }
+
 }
